8e832b7977ed4018484d865380aac591a7404401
William Patton <will@pattonwebz.com>
2021-03-20 16:33:56 +0000

    Adjust about page text

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,8 +5,8 @@ const AboutPage = () => {
             <section className="inner-container">
                 <h2 className="page-title">About DadJokez</h2>
                 <div>
-                    <p>This site is made just for a joke.</p>
-                    <p>It pulls jokes from the icanhazdadjoke.com API.</p>
+                    <p>This site is made just for a joke. It pulls jokes from the icanhazdadjoke.com API.</p>
+                    <p>It was a fun way to get a practical use for React as I do not use it day-to-day.</p>
                 </div>
             </section>
         </>
