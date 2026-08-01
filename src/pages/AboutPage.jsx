72f6e0d65f352dd497aa3199943522b1e43a0a96
William Patton <will@pattonwebz.com>
2026-08-01 03:18:53 +0100

    Tighten the about page wording

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
             <h2 className="page-title">About DadJokez</h2>
             <div>
                 <p>This site is made just for a joke. It pulls jokes from the icanhazdadjoke.com API.</p>
-                <p>It started as an excuse to get some practical use out of React, back when I did not touch it day-to-day. That part has changed since, so these days it is here purely to tell jokes.</p>
+                <p>It started as an excuse to get some practical use out of React, when I rarely used it for work. That has changed since, so these days it is here purely to tell jokes.</p>
             </div>
         </section>
     );
